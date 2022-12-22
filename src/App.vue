d6172cc1535424a7a6c76fe68f3f5d9386708678
Hasan Kılıcı <105741983+Hasan-Kilici@users.noreply.github.com>
2022-12-22 06:25:51 +0300

    Create App.vue

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<script setup>
+ import Github from './components/Github.vue';
+</script>
+
+<template>
+ <Github />
+</template>
+
+<style scoped>
+ *{
+   margin:0;
+   padding:0;
+   box-sizing:border-box;
+ }
+</style>
